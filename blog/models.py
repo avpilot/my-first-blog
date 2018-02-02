d2a2ecb8f6afdcedff1f157d6be5359217170e51
Andrey Pronin <avpronin86@gmail.com>
2018-02-02 16:58:06 +0300

    Moderating comments

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def approved_comments(self):
+		return self.comments.filter(approved_comment=True)
+
 class Comment(models.Model):
 	post = models.ForeignKey('blog.Post', related_name='comments')
 	author = models.CharField(max_length=200)
@@ -22,9 +25,9 @@ class Comment(models.Model):
 	created_date = models.DateTimeField(default=timezone.now)
 	approved_comment = models.BooleanField(default=False)
 
-def approve(self):
-	self.approved_comment = True
-	self.save()
+	def approve(self):
+		self.approved_comment = True
+		self.save()
 
-def __str__(self):
-	return self.text
+	def __str__(self):
+		return self.text
